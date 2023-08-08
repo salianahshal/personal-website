1fb4361c2d86a73a107990fedb0917b8d5c65f7c
salianahshal <salianahshal21@gmail.com>
2023-08-07 21:18:05 -0400

    updated title animation

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ const typed = new Typed(".multiple-text", {
   backDelay: 1200,
   loop: true,
 });
-
-const skills = document.querySelector("ul.skills"),
-  total_skills = skills.children.length;
-document.documentElement.style.setProperty("--total-skill", total_skills);
-
-for (let i = 0; i < total_skills; i++) {
-  skills.appendChild(skills.children[i].cloneNode(true));
-}
